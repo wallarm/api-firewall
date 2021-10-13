2986c4c6d163e8e0de02c1740250d9471cc3c806
Nikolay Tkachenko <ntkachenko@wallarm.com>
2021-10-13 16:20:18 +0700

    Update docker dir structure. Update update.sh script.

--- docker/update.sh
+++ docker/update.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-echo $BASH_SOURCE
-
-cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
-
-versions=( "$@" )
-if [ ${#versions[@]} -eq 0 ]; then
-	versions=( */ )
+if [[ $# -eq 0 ]]; then
+    echo "Current API-Firewall version not supplied"
+    exit 1
 fi
-versions=( "${versions[@]%/}" )
+
+version="$1"
 
 allVersions="$(
 	git ls-remote --tags https://github.com/wallarm/api-firewall.git \
@@ -19,17 +16,22 @@ allVersions="$(
 		| sort -rV
 )"
 
-for version in "${versions[@]}"; do
-	srcDownloadSha256="$(
-		curl -fsSL "https://github.com/wallarm/api-firewall/releases/download/v${version}/api-firewall_linux-amd64" \
-			| sha256sum \
-			| cut -d' ' -f1
-	)"
-	echo "  - v${version}: $srcDownloadSha256"
 
-	sed -ri \
-		-e 's/^(ENV\s+APIFIREWALL_VERSION\s+).*/\1'"$version"'/' \
-		-e 's/^(ENV\s+APIFIREWALL_URL\s+).*/\1'"https://github.com/wallarm/api-firewall/releases/download/v${version}/api-firewall_linux-amd64"'/' \
-		-e 's/^(ENV\s+APIFIREWALL_SHA256\s+).*/\1'"$srcDownloadSha256"'/' \
-		"$version/Dockerfile"
-done
+if [[ "${allVersions[*]}" =~ "${version}" ]]; then
+        srcDownloadSha256="$(
+                curl -fsSL "https://github.com/wallarm/api-firewall/releases/download/v${version}/api-firewall_linux-amd64" \
+                        | sha256sum \
+                        | cut -d' ' -f1
+        )"
+        echo "  - v${version}: $srcDownloadSha256"
+
+        sed -ri \
+                -e 's/^(ENV\s+APIFIREWALL_VERSION\s+).*/\1'"$version"'/' \
+                -e 's/^(ENV\s+APIFIREWALL_URL\s+).*/\1'"https://github.com/wallarm/api-firewall/releases/download/v${version}/api-firewall_linux-amd64"'/' \
+                -e 's/^(ENV\s+APIFIREWALL_SHA256\s+).*/\1'"$srcDownloadSha256"'/' \
+                "Dockerfile"
+else
+   echo "Please pass correct API-Firewall release version in argument"
+   exit 1
+fi
+
