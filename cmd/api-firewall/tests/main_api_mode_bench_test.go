71ba50cd75e9a1cf8052524bd9abb35b791a1c2b
Nikolay Tkachenko <ntkachenko@wallarm.com>
2023-10-02 19:10:57 +0300

    Add bench test for the api mode

---
+++ cmd/api-firewall/tests/main_api_mode_bench_test.go
@@ -0,0 +1,85 @@
+package tests
+
+import (
+	"bytes"
+	"encoding/json"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/valyala/fasthttp"
+	handlersAPI "github.com/wallarm/api-firewall/cmd/api-firewall/internal/handlers/api"
+	"github.com/wallarm/api-firewall/internal/platform/database"
+	"github.com/wallarm/api-firewall/internal/platform/web"
+)
+
+func BenchmarkAPIModeBasic(b *testing.B) {
+
+	//mockCtrl := gomock.NewController(b)
+	//defer mockCtrl.Finish()
+
+	//dbSpec := database.NewMockDBOpenAPILoader(mockCtrl)
+
+	logger := logrus.New()
+	logger.SetLevel(logrus.ErrorLevel)
+
+	var lock sync.RWMutex
+
+	//swagger, err := openapi3.NewLoader().LoadFromData([]byte(apiModeOpenAPISpecAPIModeTest))
+	//if err != nil {
+	//	b.Fatalf("loading swagwaf file: %s", err.Error())
+	//}
+
+	// load spec from the database
+	specStorage, err := database.NewOpenAPIDB(logger, "../../../resources/test/database/wallarm_api.db")
+	if err != nil {
+		b.Fatalf("trying to load API Spec value from SQLLite Database : %v\n", err.Error())
+	}
+
+	//dbSpec.EXPECT().SchemaIDs().Return([]int{DefaultSchemaID}).AnyTimes()
+	//dbSpec.EXPECT().Specification(DefaultSchemaID).Return(swagger).AnyTimes()
+	//dbSpec.EXPECT().SpecificationVersion(DefaultSchemaID).Return(DefaultSpecVersion).AnyTimes()
+	//dbSpec.EXPECT().IsLoaded(DefaultSchemaID).Return(true).AnyTimes()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	handler := handlersAPI.Handlers(&lock, &cfg, shutdown, logger, specStorage)
+
+	p, err := json.Marshal(map[string]interface{}{
+		"firstname": "test",
+		"lastname":  "test",
+		"job":       "test",
+		"email":     "test@wallarm.com",
+		"url":       "http://wallarm.com",
+	})
+
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	// basic test
+	b.Run("api_basic", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			req := fasthttp.AcquireRequest()
+			req.SetRequestURI("/test/signup")
+			req.Header.SetMethod("POST")
+			req.SetBodyStream(bytes.NewReader(p), -1)
+			req.Header.SetContentType("application/json")
+			req.Header.Add(web.XWallarmSchemaIDHeader, "2")
+
+			reqCtx := fasthttp.RequestCtx{
+				Request: *req,
+			}
+			handler(&reqCtx)
+			if reqCtx.Response.StatusCode() != 200 {
+				b.Errorf("Incorrect response status code. Expected: 200 and got %d",
+					reqCtx.Response.StatusCode())
+			}
+		}
+	})
+
+}
